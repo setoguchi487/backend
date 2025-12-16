733b286b8235e461ca17d6e3658b9277f751da73
setoguchi487 <setoguchi.yuya.673@s.kyushu-u.ac.jp>
2025-12-16 13:37:54 +0900

    modify

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT ?? 8000);
+  const port = process.env.PORT || 8000;
+
+  console.log('linstening on port ${port}');
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
