8c97202592c3b504c5fb06a6342538ce91a78cc2
setoguchi487 <setoguchi.yuya.673@s.kyushu-u.ac.jp>
2026-01-06 11:35:53 +0900

    modify20200106

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 8000);
-  const port = process.env.PORT || 8000;
-
-  console.log('linstening on port ${port}');
-  await app.listen(port, '0.0.0.0');
-
+  
   app.enableCors({
-  origin: [
-    'http://localhost:5173',
-    'https://https://frontend-0by4.onrender.com',
-  ],
-  credentials: true,
-});
+    origin: [
+      'http://localhost:5173',
+      'https://frontend-0by4.onrender.com',
+    ],
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 8000;
+  console.log(`listening on port ${port}`);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
